3b081c0bfec9f5414dc1a2615f32a75dd3f9d37f
aravind-3697 <43001827+aravind-3697@users.noreply.github.com>
2025-07-30 22:25:29 +0530

    Update weatherreport.py

--- weatherreport.py
+++ weatherreport.py
@@ -44,6 +44,14 @@ def cloudy_check():
         'windSpeedKMPH': 20
     }
 
+def stormy_with_rain_check():
+    return {
+        'temperatureInC': 20,
+        'precipitation': 90,  
+        'humidity': 80,
+        'windSpeedKMPH': 60
+    }
+
 def test_rainy_condition():
     weather = report(rainy_check)
     assert "rain" in weather.lower()
@@ -56,8 +64,13 @@ def test_cloudy_condition():
     weather = report(cloudy_check)
     assert "cloudy" in weather.lower()
 
+def test_stormy_with_rain_condition():
+    weather = report(stormy_with_rain_check)
+    assert "storm" in weather.lower()
+
 if __name__ == '__main__':
     test_rainy_condition()
     test_storm_condition()
     test_cloudy_condition()
+    test_stormy_with_rain_condition()
     print("All is well...")
